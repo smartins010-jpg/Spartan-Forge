fdbfcb655f6d192c2cf7ba2a4d1fdd84baf67b31
smartins010-jpg <smartins010@gmail.com>
2026-07-16 21:29:04 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='spartan-forge-github-v567-pwa';
-const CORE=[
+const CACHE='spartan-forge-v568';
+const APP_SHELL=[
   './',
   './index.html',
   './manifest.webmanifest',
@@ -8,41 +8,41 @@ const CORE=[
   './icon-maskable-512.png'
 ];
 
-self.addEventListener('install',event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)));
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate',event=>{
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys=>
-      Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))
     )
   );
   self.clients.claim();
 });
 
-self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET')return;
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
 
-  if(event.request.mode==='navigate'){
+  if (event.request.mode === 'navigate') {
     event.respondWith(
-      fetch(event.request)
-        .then(response=>{
-          const copy=response.clone();
-          caches.open(CACHE).then(cache=>cache.put('./index.html',copy));
+      fetch(event.request, {cache:'no-store'})
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put('./index.html', copy));
           return response;
         })
-        .catch(()=>caches.match('./index.html'))
+        .catch(() => caches.match('./index.html'))
     );
     return;
   }
 
   event.respondWith(
-    caches.match(event.request).then(cached=>
-      cached || fetch(event.request).then(response=>{
-        const copy=response.clone();
-        caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+    caches.match(event.request).then(cached =>
+      cached || fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
         return response;
       })
     )
