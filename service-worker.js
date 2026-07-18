4d9a8e92a6ba0117570958fe50d857ee0c850c82
smartins010-jpg <smartins010@gmail.com>
2026-07-18 16:30:06 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='spartan-forge-v10-gps-auto-pause-5000';
+const CACHE='spartan-forge-v10-1';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-512.png','./hero-top-v8.jpg','./strava-config.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
